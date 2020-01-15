6679c4df8b858e27cee9461ecce2961176ef93b7
Alexandre <alexandrecardosodebarros1@outlook.com>
2020-01-15 14:47:43 -0300

    Criei a cadastro e forms do cachorro

--- cachorro/views.py
+++ cachorro/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from cachorro.forms import *
 
 # Create your views here.
+
+def cadastro(request):
+    form = CachorroForm(request.POST or None)
+    args = {
+        'form':form
+    }
+    if form.is_valid():
+        form.save()
+        args['msg'] = 'Cadastro Realizado com sucesso'
+        return redirect ('/')
+    return render(request, 'cadastro.html', args)
