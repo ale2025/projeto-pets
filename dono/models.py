544ba97c7a5259ed03cf875f03bb1a9f00a7be99
Alexandre <alexandrecardosodebarros1@outlook.com>
2020-01-15 10:46:47 -0300

    auterei cores do gatinho

--- dono/models.py
+++ dono/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here
 class Dono(models.Model):
     nome = models.CharField(max_length=255)
-
+    endereco = models.CharField(max_length=255)
+    telefone = models.IntegerField()
     def __str__(self):
         return self.nome
--- gatinho/models.py
+++ gatinho/models.py
@@ -7,10 +7,14 @@ from dono.models import Dono
 class Gatinho(models.Model):
     nome = models.CharField(max_length=255)
     idade = models.IntegerField()
-    cores = (
+    cores_do_pelo = (
+        ('MARRON','Marron'),
+        ('CINZA','Cinza'),
+        ('LARANJA','Laranja'),
+        ('BEGE','Bege'),
         ('PRETO', 'Preto'),
         ('BRANCO', 'Branco'),
         ('PARDO', 'Pardo'),
     )
-    cor = models.CharField(choices=cores, max_length=100)
-    dono = models.ManyToManyField(Dono, null=True)
+    cor_do_pelo = models.CharField(choices=cores, max_length=100)
+    dono = models.ManyToManyField(Dono)
